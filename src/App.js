6543454142f42e32ded686b3f7f27f58cef8d837
Richa <r.shah@axelor.com>
2023-01-09 19:44:54 +0530

    Add basic tree structure with data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React, { useState, useEffect } from "react";
 import OrgChartEditor from "./components/OrgChartEditor";
 
+const getParams = () => {
+  const params = new URL(document.location).searchParams;
+  return {
+    id: params.get("id"),
+  };
+};
+
 function App() {
+  const [parameters, setParameters] = useState({});
+
+  useEffect(() => {
+    setParameters(getParams());
+  }, []);
+
   return (
     <section className="dhx-container">
-      <OrgChartEditor />
+      <OrgChartEditor parameters={parameters} />
     </section>
   );
 }
--- src/components/OrgChartEditor/index.js
+++ src/components/OrgChartEditor/index.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import fromCDN from "from-cdn";
+import _ from "loadsh";
 import Button from "../Button";
-import { medicalWorkers } from "../../static/data";
+import Service from "../../services";
 
 import "./index.css";
+
+const getImageURL = (photo) => {
+  const { id } = photo || {};
+  if (!id) {
+    return '';
+  }
+  return `${
+    process.env.NODE_ENV === "production" ? ".." : "axelor-erp"
+  }/ws/rest/com.axelor.meta.db.MetaFile/${id}/content/download`;
+};
 
 const template = ({ photo, name, post, phone, mail }) => {
   return `
     <div class="dhx_diagram_template_a_box dhx_diagram_template_a">
       <div class="dhx_diagram_template_a__inside">
-        <div class="dhx_diagram_template_a__picture" style="background-image: url(${photo});"></div>
+        <div class="dhx_diagram_template_a__picture" style="background-image: url(${getImageURL(
+          photo
+        )});">
+        </div>
         <div class="dhx_diagram_template_a__body">
           <div class="dhx_diagram_template_a__title">${name}</div>
           <div class="dhx_diagram_template_a__row">
@@ -31,14 +45,146 @@ const template = ({ photo, name, post, phone, mail }) => {
   `;
 };
 
-export default function OrgChartEditor() {
-  const [collapse, setCollapse] = React.useState(true);
-  const diagramContainer = React.useRef();
-  const editorContainer = React.useRef();
-  const diagramRef = React.useRef(null);
-  const editorRef = React.useRef(null);
+export default function OrgChartEditor({ parameters = {} }) {
+  const { id } = parameters;
+  const [collapse, setCollapse] = useState(true);
+  const [planning, setPlanning] = useState(true);
+  const [employees, setEmployees] = useState(true);
+  const diagramContainer = useRef();
+  const editorContainer = useRef();
+  const diagramRef = useRef(null);
+  const editorRef = useRef(null);
 
-  React.useEffect(() => {
+  function handleEdit() {
+    setCollapse(false);
+    const editor = editorRef.current;
+    const diagram = diagramRef.current;
+    editor && diagram && editor.import(diagram);
+  }
+
+  useEffect(() => {
+    if (!id) return;
+    (async () => {
+      const planning = await Service.search(
+        "com.axelor.apps.hr.db.OrganizationChart",
+        {
+          data: {
+            criteria: [
+              {
+                fieldName: "id",
+                operator: "=",
+                value: id,
+              },
+            ],
+          },
+          fields: ["company", "companyDepartmentSet", "company.headOfCompany"],
+        }
+      );
+      if (planning?.status === 0) {
+        setPlanning(planning?.data[0]);
+      }
+    })();
+  }, [id]);
+
+  useEffect(() => {
+    if (!planning) return;
+    (async () => {
+      const { company, companyDepartmentSet } = planning;
+      let allEmployees = [];
+      const getEmployees = async (data) => {
+        const res = await Service.search("com.axelor.apps.hr.db.Employee", {
+          data,
+          fields: [
+            "contactPartner.emailAddress.address",
+            "contactPartner.picture",
+            "contactPartner.firstName",
+            "contactPartner.name",
+            "contactPartner.simpleFullName",
+            "contactPartner.fixedPhone",
+            "contactPartner.mobilePhone",
+            "managerUser",
+            "managerUser.employee.id",
+            "mainEmploymentContract.companyDepartment",
+            "mainEmploymentContract.payCompany",
+          ],
+        });
+        return res?.status === 0 ? res?.data : [];
+      };
+      const getManagers = async (employees = []) => {
+        const managers = employees?.filter((e) => e.managerUser);
+        allEmployees = [...allEmployees, ...(employees || [])];
+        const managerIds = managers.map((m) => m.id);
+        if (managerIds.length > 0) {
+          let criteria = [
+            {
+              fieldName: "managerUser.id",
+              operator: "IN",
+              value: managerIds,
+            },
+            {
+              fieldName: "mainEmploymentContract.payCompany.id",
+              operator: "=",
+              value: company?.id,
+            },
+          ];
+          if (companyDepartmentSet?.length > 0) {
+            criteria.push({
+              fieldName: "mainEmploymentContract.companyDepartment.id",
+              operator: "IN",
+              value: companyDepartmentSet.map((c) => c.id),
+            });
+          }
+          const newemps = await getEmployees({
+            criteria,
+            operator: "and",
+          });
+          allEmployees = [...allEmployees, ...(newemps || [])];
+          getManagers(newemps);
+        }
+      };
+
+      const headOfCompany = planning && planning["company.headOfCompany"];
+      if (!headOfCompany) return;
+      let payload = [
+        {
+          fieldName: "mainEmploymentContract.payCompany.id",
+          operator: "=",
+          value: company?.id,
+        },
+        {
+          operator: "or",
+          criteria: [
+            {
+              fieldName: "id",
+              operator: "=",
+              value: headOfCompany?.id,
+            },
+            {
+              fieldName: "managerUser.employee.id",
+              operator: "=",
+              value: headOfCompany?.id,
+            },
+          ],
+        },
+      ];
+      if (companyDepartmentSet?.length > 0) {
+        payload.push({
+          fieldName: "mainEmploymentContract.companyDepartment.id",
+          operator: "IN",
+          value: companyDepartmentSet.map((c) => c.id),
+        });
+      }
+      const employees = await getEmployees({
+        criteria: payload,
+        operator: "and",
+      });
+      await getManagers(employees);
+      setEmployees(allEmployees);
+    })();
+  }, [planning]);
+
+  useEffect(() => {
+    if (!employees.length) return;
     fromCDN([
       "https://webix.io/dev/dhtmlx/diagram/diagram_4.0/codebase/diagramWithEditor.css",
       "https://webix.io/dev/dhtmlx/diagram/diagram_4.0/codebase/diagramWithEditor.js",
@@ -74,7 +220,6 @@ export default function OrgChartEditor() {
           post: "Position held",
           phone: "(405) 000-00-00",
           mail: "some@mail.com",
-          photo: "../common/big_img/big-avatar-1.jpg",
           height: 115,
           width: 330,
         },
@@ -96,8 +241,18 @@ export default function OrgChartEditor() {
       editor.events.on("ResetButton", () => {
         setCollapse(false);
       });
-
-      diagram.data.parse(medicalWorkers);
+      const emp = employees?.map((emp) => {
+        return {
+          id: emp?.id,
+          name: emp["contactPartner.simpleFullName"],
+          post: emp["mainEmploymentContract.companyDepartment"]?.name,
+          phone: emp["contactPartner.fixedPhone"],
+          mail: emp["contactPartner.emailAddress.address"],
+          photo: emp["contactPartner.picture"],
+          parent: emp["managerUser.employee.id"],
+        };
+      });
+      diagram.data.parse(_.uniqBy(emp, "id") || []);
 
       diagramRef.current = diagram;
       editorRef.current = editor;
@@ -107,14 +262,7 @@ export default function OrgChartEditor() {
       const diagram = diagramRef.current;
       diagram && diagram.destructor();
     };
-  }, []);
-
-  function handleEdit() {
-    setCollapse(false);
-    const editor = editorRef.current;
-    const diagram = diagramRef.current;
-    editor && diagram && editor.import(diagram);
-  }
+  }, [employees]);
 
   return (
     <div
